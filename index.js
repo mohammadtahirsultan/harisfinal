332a61bf5f8c641f2c678f395aa807209080597b
Rao Khuram Shahzad <71884500+KhuramShahzad-5335@users.noreply.github.com>
2023-12-29 01:08:38 +0500

    made the fare-rules request to complete this sucker

--- index.js
+++ index.js
@@ -20,57 +20,56 @@ app.use(express.json());
 
 
 
-// Create a route to handle SOAP requests
-app.get('/galileo-available', async (req, response) => {
-    let params = {
-        legs: [
-            {
-                from: 'WAW',
-                to: 'AMS',
-                departureDate: '2023-12-29',
-            },
-        ],
-        passengers: {
-            ADT: 1,
-            /*
-             CNN:1,
-             INF: 1,
-             INS: 1, //infant with a seat
-             */
-        },
-        permittedCarriers: ['LO'],
-        cabins: ['Economy'], // ['Business'],
-        bookingCodes: ['Q', 'G'],
-        requestId: '4e2fd1f8-2221-4b6c-bb6e-cf05c367cf60',
-        // permittedConnectionPoints: ['AMS'],
-        // preferredConnectionPoints: ['KBP'],
-        // prohibitedConnectionPoints: ['DME', 'SVO', 'PAR'],
-        // maxJourneyTime: 300,
-        pricing: {
-            currency: 'USD',
-            eTicketability: true,
-        },
-        pricing: {
-            currency: 'USD', // Currency to convert results prices
-            eTicketability: true, // Detect if pricing solution will be ticketable as e-ticket
-        },
-        includeFare: true,
-    };
+// // Create a route to handle SOAP requests
+// app.get('/galileo-available', async (req, response) => {
+//     let params = {
+//         legs: [
+//             {
+//                 from: 'WAW',
+//                 to: 'AMS',
+//                 departureDate: '2023-12-29',
+//             },
+//         ],
+//         passengers: {
+//             ADT: 1,
+//             /*
+//              CNN:1,
+//              INF: 1,
+//              INS: 1, //infant with a seat
+//              */
+//         },
+//         permittedCarriers: ['LO'],
+//         cabins: ['Economy'], // ['Business'],
+//         bookingCodes: ['Q', 'G'],
+//         requestId: '4e2fd1f8-2221-4b6c-bb6e-cf05c367cf60',
+//         // permittedConnectionPoints: ['AMS'],
+//         // preferredConnectionPoints: ['KBP'],
+//         // prohibitedConnectionPoints: ['DME', 'SVO', 'PAR'],
+//         // maxJourneyTime: 300,
+//         pricing: {
+//             currency: 'USD',
+//             eTicketability: true,
+//         },
+//         pricing: {
+//             currency: 'USD', // Currency to convert results prices
+//             eTicketability: true, // Detect if pricing solution will be ticketable as e-ticket
+//         },
+//         includeFare: true,
+//     };
 
-    try {
-        AirService.availability(params)
-            .then(
-                res => { return response.log(res); },
-                err => { return response.json(err) }
-            );
-    } catch (error) {
-        response.status(500).send('Internal Server Error', error.message);
-    }
-});
+//     try {
+//         AirService.availability(params)
+//             .then(
+//                 res => { return response.log(res); },
+//                 err => { return response.json(err) }
+//             );
+//     } catch (error) {
+//         response.status(500).send('Internal Server Error', error.message);
+//     }
+// });
 
-// Create a route to handle SOAP requests
+//1.  Create a route to handle SOAP requests, to fetch the available flights and fares.
 app.get('/shop', async (req, response) => {
-
     const params = {
         legs: [
             {
@@ -78,157 +77,67 @@ app.get('/shop', async (req, response) => {
                 to: 'DXB',
                 departureDate: '2024-03-30'
             },
-            {
-                from: 'MUX',
-                to: 'LHR',
-                departureDate: '2024-03-30',
-            },
         ],
         passengers: {
             ADT: 1,
-            /*
-             CNN:1,
-             INF: 1,
-             INS: 1, //infant with a seat
-             */
         },
-        cabins: ['Economy'], // ['Business'],
-        requestId: '4e2fd1f8-2221-4b6c-bb6e-cf05c367cf60',
-        maxJourneyTime: 300,
-        maxSolutions: 200,
+        cabins: ['Economy'],
         pricing: {
-            currency: 'USD',
-            // eTicketability: true,
+            currency: 'PKR',
         },
+        includeFare: true,
     };
-
-
-    AirService.shop(params).then(
-        data => { return response.json(data) },
-        err => { return response.json(err) }
-    );
-
-});
-
-// Create a route to handle SOAP requests
-app.get('/flight-info', async (req, response) => {
-    const params = {
-        airline: 'OS',
-        flightNumber: '703',
-        departure: '2023-12-30',
-    };
-
-    AirService.flightInfo(params).then(
-        data => { return response.json(data) },
-        err => { return response.json(err) }
-    );
-    try {
-        AirService.availability(params)
-            .then(
-                res => { return response.json(res); },
-                err => { return response.json(err) }
-            );
-    } catch (error) {
-        response.status(500).send('Internal Server Error', error.message);
-    }
-});
-
-
-
-app.get('/galileo-fares', async (req, response) => {
-    try {
-        // Create a new instance of the fare service
-        const FareService = uAPI.createFareService({
-            auth: config,
-            debug: 2,
-            production: true,
-        });
-
-        // Define parameters for the fare request
-        const fareParams = {
-            // Specify the fare-related parameters (e.g., fare basis code, passenger type, etc.)
-            // ...
-        };
-
-        // Make a request to the fare service
-        FareService.getFareInfo(fareParams)
-            .then(
-                res => { return response.json(res) },
-                err => { return response.json(err) }
-            );
-    } catch (error) {
-        response.status(500).send('Internal Server Error', error.message);
-    }
-});
-
-
-
-
-params = {
-    legs: [
-        {
-            from: 'WAW',
-            to: 'AMS',
-            departureDate: '2023-12-29',
-        },
-        // {
-        //     from: 'PAR',
-        //     to: 'IEV',
-        //     departureDate: '2023-12-30',
-        // },
-    ],
-    passengers: {
-        ADT: 1,
-    },
-    // requestId: 'test-request',
-    // platingCarier: 'PS'
-};
-
-app.get('/book', async (req, response) => {
 
     AirService.shop(params)
         .then(
-            (results) => {
-                const fromSegments = results['0'].directions['0']['0'].segments;
-                const toSegments = results['0'].directions['1']['0'].segments;
-
-                const book = {
-                    segments: fromSegments.concat(toSegments),
-                    rule: 'SIP',
-                    passengers: [{
-                        lastName: 'SKYWALKER',
-                        firstName: 'ANAKIN',
-                        passCountry: 'UA',
-                        passNumber: 'ES221731',
-                        birthDate: '1968-07-25',
-                        gender: 'M',
-                        ageCategory: 'ADT',
-                    }],
-                    phone: {
-                        countryCode: '38',
-                        location: 'IEV',
-                        number: '0660419905',
-                    },
-                    deliveryInformation: {
-                        name: 'Anakin Skywalker',
-                        street: 'Sands street, 42',
-                        zip: '42042',
-                        country: 'Galactic Empire',
-                        city: 'Mos Eisley',
-                    },
-                    allowWaitlist: true,
-                };
-
-                return AirService.book(book).then(
-                    res => { return response.json(res) },
-                    err => { return response.json(err) }
-                );
-            }
+            data => {
+                response.json({ shopData: data });
+            },
+            err => response.json({ shopDataError: err })
         )
-        .catch(
-            err => { return response.json(err) }
+        .catch(err => {
+            console.error(err);
+            response.json({ error: 'An unexpected error occurred.' });
+        });
+});
+
+
+
+
+
+// 2. now we have to book the flight
+// before booking we need to fetch the fare rules, to check if the flight is refundable or not and other things too
+app.get('/fare-rules', async (req, response) => {
+    // Assume you have received the shop data or fetch it from the shop API
+    const shopDataResponse = await fetch('http://localhost:5000/shop');
+    const shopData = await shopDataResponse.json();
+    
+    // Extract necessary information
+    const forwardSegments = shopData['0'].directions['0']['0'].segments;
+    const backSegments = shopData['0'].directions['1']['0'].segments;
+
+    const fareRulesParams = {
+        segments: forwardSegments.concat(backSegments),
+        passengers: { ADT: 1 }, // Update with actual passenger information
+        long: true,
+        requestId: 'test',
+    };
+
+    // Make the fare rules request
+    AirService.fareRules(fareRulesParams)
+        .then(
+            fareRules => response.json({ fareRules : fareRules }),
+            err => response.json({ fareRulesError: err })
         )
-})
+        .catch(err => {
+            console.error(err);
+            response.json({ error: 'An unexpected error occurred.' });
+        });
+});
+
+
+
+// 3. now we have to book the flight
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
