1eab7acac1e0b0089b7bca7b49ccf81bef74dbe6
Rao Khuram Shahzad <71884500+KhuramShahzad-5335@users.noreply.github.com>
2023-12-29 19:48:58 +0500

    chutyaap

--- index.js
+++ index.js
@@ -48,62 +48,18 @@ app.post('/shop-oneway', async (req, response) => {
             pricing: {
                 currency: 'PKR',
             },
-            includeFareInfo: true,
-            async: true,
         };
 
         const data = await AirService.shop(params);
         return response.json(data);
     } catch (err) {
         console.error('Error:', err);
-        return response.status(500).json({ error: `${err.name} `});
+        return response.status(500).json({ error: ${err.name} });
     }
 });
 
 // -----------------   ROUND TRIP REQUEST  -----------------------
 app.post('/shop-rounded', async (req, response) => {
-    try {
-        const { from, to, departureDate, returnDate } = req.query;
-
-        if (!from || !to || !departureDate || !returnDate) {
-            return response.status(400).json({ error: 'Missing required parameters' });
-        }
-
-        const params = {
-            legs: [
-                {
-                    from,
-                    to,
-                    departureDate,
-                },
-                {
-                    from: to, // Swap 'from' and 'to' for the return leg
-                    to: from,
-                    departureDate: returnDate, // Use the provided returnDate
-                },
-            ],
-            passengers: {
-                ADT: 1,
-            },
-            cabins: ['Economy'],
-            pricing: {
-                currency: 'PKR',
-                // eTicketability: true,
-            },
-        };
-
-        const data = await AirService.shop(params);
-        return response.json(data);
-    } catch (err) {
-        console.error('Error:', err);
-        return response.status(500).json({ error: 'An error occurred' });
-    }
-});
-
-
-
-// -----------------   Multi City Request  -----------------------
-app.post('/shop-multicity', async (req, response) => {
     try {
         const { from, to, departureDate, returnDate } = req.query;
 
