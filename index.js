0f5f2246005ab8209da8c31b2287f7c3e197334d
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-12-29 19:44:18 +0500

    one way and rounded frontend and backend done

--- index.js
+++ index.js
@@ -2,89 +2,103 @@ const uAPI = require('./entry');
 const config = require('./config');
 const express = require('express');
 const cors = require("cors")
-const book = require("./book")
+const book = require("./book");
+const bodyParser = require('body-parser');
 
 const app = express()
+
+// MiddleWares 
 app.use(cors({
     origin: "http://localhost:5173"
 }))
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+// Starting the Game of Travelport 
 let AirService = uAPI.createAirService(
     {
         auth: config,
         debug: 2,
         production: true,
     }
-);
-app.use(express.json());
+)
 
 
 // -----------------   ONE WAY REQUEST  -----------------------
 //1.  Create a route to handle SOAP requests, to fetch the available flights and fares.
-app.get('/shop-oneway', async (req, response) => {
-    const params = {
-        legs: [
-            {
-                from: 'MUX',
-                to: 'DXB',
-                departureDate: '2024-03-30'
+app.post('/shop-oneway', async (req, response) => {
+    try {
+        console.log("I am body", req.body);
+        let { from, to, departureDate } = req.body;
+
+        if (!from || !to || !departureDate) {
+            return response.status(400).json({ error: 'Missing required parameters' });
+        }
+        const params = {
+            legs: [
+                {
+                    from,
+                    to,
+                    departureDate,
+                },
+            ],
+            passengers: {
+                ADT: 1,
             },
-            {
-                from: 'MUX',
-                to: 'LHR',
-                departureDate: '2024-03-30',
+            cabins: ['Economy'],
+            pricing: {
+                currency: 'PKR',
             },
-        ],
-        passengers: {
-            ADT: 1,
-        },
-        cabins: ['Economy'],
-        pricing: {
-            currency: 'USD',
-            // eTicketability: true,
-        },
-    };
+        };
 
-
-    AirService.shop(params).then(
-        data => { return response.json(data) },
-        err => { return response.json(err) }
-    );
-
+        const data = await AirService.shop(params);
+        return response.json(data);
+    } catch (err) {
+        console.error('Error:', err);
+        return response.status(500).json({ error: `${err.name}` });
+    }
 });
 
-
 // -----------------   ROUND TRIP REQUEST  -----------------------
-app.get('/shop-rounded', async (req, response) => {
-    const params = {
-        legs: [
-            {
-                from: 'MUX',
-                to: 'DXB',
-                departureDate: '2024-03-30'
+app.post('/shop-rounded', async (req, response) => {
+    try {
+        const { from, to, departureDate, returnDate } = req.body;
+
+        if (!from || !to || !departureDate || !returnDate) {
+            return response.status(400).json({ error: 'Missing required parameters' });
+        }
+
+        const params = {
+            legs: [
+                {
+                    from,
+                    to,
+                    departureDate,
+                },
+                {
+                    from: to, // Swap 'from' and 'to' for the return leg
+                    to: from,
+                    departureDate: returnDate, // Use the provided returnDate
+                },
+            ],
+            passengers: {
+                ADT: 1,
             },
-            {
-                from: 'DXB',
-                to: 'MUX', // Return leg
-                departureDate: '2024-04-05', // Adjust the return date as needed
+            cabins: ['Economy'],
+            pricing: {
+                currency: 'PKR',
+                // eTicketability: true,
             },
-        ],
+        };
 
-        passengers: {
-            ADT: 1,
-        },
-        cabins: ['Economy'],
-        pricing: {
-            currency: 'USD',
-            // eTicketability: true,
-        },
-    };
-
-
-    AirService.shop(params).then(
-        data => { return response.json(data) },
-        err => { return response.json(err) }
-    );
-
+        const data = await AirService.shop(params);
+        return response.json(data);
+    } catch (err) {
+        console.error('Error:', err);
+        return response.status(500).json({ error: 'An error occurred' });
+    }
 });
 
 
@@ -94,7 +108,7 @@ app.get('/fare-rules', async (req, response) => {
     // Assume you have received the shop data or fetch it from the shop API
     const shopDataResponse = await fetch('http://localhost:5000/shop');
     const shopData = await shopDataResponse.json();
-    
+
     // Extract necessary information
     const forwardSegments = shopData['0'].directions['0']['0'].segments;
     const backSegments = shopData['0'].directions['1']['0'].segments;
@@ -109,7 +123,7 @@ app.get('/fare-rules', async (req, response) => {
     // Make the fare rules request
     AirService.fareRules(fareRulesParams)
         .then(
-            fareRules => response.json({ fareRules : fareRules }),
+            fareRules => response.json({ fareRules: fareRules }),
             err => response.json({ fareRulesError: err })
         )
         .catch(err => {
